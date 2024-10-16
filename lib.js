116c6e282dcdf7de3f1fcd3bb00521b3414c366a
MOK <kimmgm0528@gmail.com>
2024-10-16 11:07:28 +0900

    testing

---
+++ lib.js
@@ -0,0 +1,29 @@
+function avg(numbers){
+    let s = numbers.reduce((prev, curr) => prev + curr,0);
+        return s / numbers.length;
+}
+
+function prime(num) {
+    if (num == 2){
+        return true;
+    }
+    for (i = 2; i < num ; i++){
+        if (num%i == 0){
+            return true;
+        }
+    }
+    return false;
+
+}
+function factorial(num){
+    let result = 1;
+    for (let i = 1 ; i <= num ; i++){
+        result = result * i;
+    }
+    return result;
+}
+module.exports = {
+    avg,
+    prime,
+    factorial
+}
---
+++ main.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+
+
+const lib = require("./lib");
+if (process.argc.length <= 3){
+    console.log("INSUFFICIENT PARAMETER!");
+    process.exit(1);
+}
+let command = process.argv[2];
+
+let numbers = process.argv
+    .slice(3, process.argv.length)
+    .map((n)=> parseFloat(n));
+if (numbers.some((n)=> isNaN(n))){
+    console.log("Some arguments are not numbers!");
+    process.exit(1);
+}
+
+    let result;
+switch (command){
+    case "avg":
+        result = lib.avg(numbers);
+        console.log("result is : ");
+        break;
+    case "prime":
+        result = lib.prime(numbers[0]);
+        if (result){
+            console.log("NOT PRIME");
+        }
+        else {
+            console.log("PRIME");
+        }
+        break;
+    case "factorial":
+        result = lib.factorial(numbers[0])
+        console.log("result is : ");
+        break;
+    default:
+        console.log("Wrong command!");
+        process.exit(1);
+}
+console.log(result);
